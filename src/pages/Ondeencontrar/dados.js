6f1603b045610bc7b050825dcfeb57d4ec93357b
Nathy Cabral <ncabral409@gmail.com>
2022-07-29 21:07:53 -0300

    Atualizando cards

--- src/pages/Ondeencontrar/dados.js
+++ src/pages/Ondeencontrar/dados.js
@@ -1,29 +1,29 @@
 const lugares = [{
         id: 1,
-        nome: "Casa 1",
-        telefone: "7777",
-        endereco: "Rua bla bla bls"
+        nome: "Casa de Cultura Itaim Paulista",
+        telefone: " (11) 2025-1950",
+        endereco: " R. Monte Camberela"
 
     },
     {
         id: 2,
-        nome: "Casa 1",
-        telefone: "7777",
-        endereco: "Rua bla bla bls"
+        nome: "Casa de Cultura Municipal de Guaianases",
+        telefone: "(11) 2016-1961",
+        endereco: " R. Castelo de Leça"
 
     },
     {
         id: 3,
-        nome: "Casa 1",
-        telefone: "7777",
-        endereco: "Rua bla bla bls"
+        nome: "ONG Benevolência Educação e Vivência",
+        telefone: "Não possui telefone.",
+        endereco: "R. Silveira Pires, 256"
 
     },
     {
         id: 4,
-        nome: "Casa 1",
-        telefone: "7777",
-        endereco: "Rua bla bla bls"
+        nome: "ONG EDUCALÊ - Aprendiz do Saber",
+        telefone: "Não possui telefone.",
+        endereco: "R. Goma de Olibano"
 
     }
 
